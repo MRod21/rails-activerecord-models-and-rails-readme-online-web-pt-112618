ad4469e96578458b99cd526ecc08862c27c93bd0
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-19 02:42:23 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 describe Post do
-
+  it 'can be create' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
 end
